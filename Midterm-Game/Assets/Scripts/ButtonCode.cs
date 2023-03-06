504d745d6dcebf7fd4f760195fbc9f52f231f3e3
scullin-hazel <hazelscullin2003@gmail.com>
2023-03-06 12:55:27 -0500

    THE MENU WORKS

--- Midterm-Game/Assets/Scripts/ButtonCode.cs
+++ Midterm-Game/Assets/Scripts/ButtonCode.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+//using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ButtonCode : MonoBehaviour
 {
+
+    //public Button startButton;
+    //public Button howToPlayButton;
+    public Button exitButton;
+    public Label controls;
+
     // Start is called before the first frame update
     void Start()
     {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        //startButton = root.Q<Button>"start-button";
+        Button startButton = root.Q<Button>("start-button");
+        Button howToPlayButton = root.Q<Button>("how-to-play");
         
+        //howToPlayButton = root.Q<Button>"how-to-play";
+        exitButton = root.Q<Button>("exit-button");
+        controls = root.Q<Label>("controls");
+
+        startButton.clicked += StartButtonPressed;
+        howToPlayButton.clicked += howToPlayButtonPressed;
     }
 
     // Update is called once per frame
-    void Update()
+    void StartButtonPressed()
     {
-        
+        SceneManager.LoadScene("JCC");
+    }
+
+    void howToPlayButtonPressed() {
+        controls.text = "Player 1: WASD to move, L shift to throw \n Player 2: arrows to move, space to throw";
+        controls.style.display = DisplayStyle.Flex;
     }
 }
