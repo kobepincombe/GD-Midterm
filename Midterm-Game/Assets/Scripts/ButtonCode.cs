0b16a1eec09282d517da9415a3899d42b7a3d77d
scullin-hazel <hazelscullin2003@gmail.com>
2023-03-06 13:00:10 -0500

    now its really working, im so proud

--- Midterm-Game/Assets/Scripts/ButtonCode.cs
+++ Midterm-Game/Assets/Scripts/ButtonCode.cs
@@ -8,35 +8,29 @@ using UnityEngine.SceneManagement;
 public class ButtonCode : MonoBehaviour
 {
 
-    //public Button startButton;
-    //public Button howToPlayButton;
-    public Button exitButton;
+    public Button startButton;
+    public Button howToPlayButton;
     public Label controls;
 
     // Start is called before the first frame update
     void Start()
     {
         var root = GetComponent<UIDocument>().rootVisualElement;
-        //startButton = root.Q<Button>"start-button";
-        Button startButton = root.Q<Button>("start-button");
-        Button howToPlayButton = root.Q<Button>("how-to-play");
-        
-        //howToPlayButton = root.Q<Button>"how-to-play";
-        exitButton = root.Q<Button>("exit-button");
+        startButton = root.Q<Button>("start-button");
+        howToPlayButton = root.Q<Button>("how-to-play");
         controls = root.Q<Label>("controls");
 
         startButton.clicked += StartButtonPressed;
         howToPlayButton.clicked += howToPlayButtonPressed;
     }
 
-    // Update is called once per frame
     void StartButtonPressed()
     {
         SceneManager.LoadScene("JCC");
     }
 
     void howToPlayButtonPressed() {
-        controls.text = "Player 1: WASD to move, L shift to throw \n Player 2: arrows to move, space to throw";
+        controls.text = "Player 1: WASD to move, \n L shift to throw \n Player 2: arrows to move, \n space to throw";
         controls.style.display = DisplayStyle.Flex;
     }
 }
